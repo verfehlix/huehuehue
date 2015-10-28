21000f6651c1c40a20b3f7ceeb3fe0114366966c
Felix <felix.p.magnus@gmail.com>
2015-10-28 18:46:01 +0100

    huehuehue

--- js/index.js
+++ js/index.js
@@ -3,34 +3,36 @@
 var gui = require('nw.gui');
 var CustomTrayMenu = require('./js/custom_tray_menu');
 
-var $ = function (selector) {
-  return document.querySelector(selector);
-}
+// var $ = function (selector) {
+//   return document.querySelector(selector);
+// }
 
 var customTray;
 
 document.addEventListener('DOMContentLoaded', function() {
-  $('#add-tray').addEventListener('click', function () {
+//   $('#add-tray').addEventListener('click', function () {
     if (!customTray) {
       customTray = new CustomTrayMenu('custom-tray-menu.html', '/img/tray.png', {
         width: 200,
         height: 1000
       });
     }
-  });
+  // });
 
-  $('#remove-tray').addEventListener('click', function () {
-    if (customTray) {
-      customTray.remove();
-      customTray = undefined;
-    }
-  });
+  // $('#remove-tray').addEventListener('click', function () {
+  //   if (customTray) {
+  //     customTray.remove();
+  //     customTray = undefined;
+  //   }
+  // });
 
   // bring window to front when open via terminal
-  gui.Window.get().focus();
+  // gui.Window.get().focus();
 
   // for nw-notify frameless windows
-  gui.Window.get().on('close', function() {
-    gui.App.quit();
-  });
+  // gui.Window.get().on('close', function() {
+  //   gui.App.quit();
+  // });
+  
+  gui.Window.get().hide();
 });
