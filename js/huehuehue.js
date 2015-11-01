af270ad6380e5dcdc30cc93108dd6d9edb011e8a
Felix <felix.p.magnus@gmail.com>
2015-11-01 16:23:23 +0100

    huehuehue

---
+++ js/huehuehue.js
@@ -0,0 +1,31 @@
+var gui = require('nw.gui');
+var win = gui.Window.get();
+
+var tray = new gui.Tray({
+	title: '',
+	icon: "/img/tray.png",
+	alticon: '',
+	tooltip: window.document.title,
+	iconsAreTemplates: false
+});
+
+tray.on('click', toggleTrayMenuAt.bind(this));
+
+// called when user click on tray icon
+function toggleTrayMenuAt(position) {
+	if (win.shown) {
+		win.hide();
+		win.shown = false;
+	} else {
+		translate(position);
+		win.moveTo(position.x, 30);
+		win.show();
+		win.focus();
+		win.shown = true;
+	}
+}
+
+function translate(pos) {
+	pos.x -= Math.floor(win.width / 2);
+	pos.y -= 0;
+}
